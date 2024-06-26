c0ca3fe44de846dafab8d11e12eed2b3899b9a79
wjj-phoenix <3951817834@qq.com>
2024-06-26 17:20:58 +0800

    doc:更新数据库脚本

--- sql/scaffold.sql
+++ sql/scaffold.sql
@@ -1,21 +1,62 @@
 /*
  Navicat Premium Dump SQL
 
- Source Server         : 192.168.156.24_3306
+ Source Server         : 192.168.73.100_3306
  Source Server Type    : MySQL
- Source Server Version : 80037 (8.0.37-0ubuntu0.24.04.1)
- Source Host           : 192.168.156.24:3306
+ Source Server Version : 80037 (8.0.37-0ubuntu0.22.04.3)
+ Source Host           : 192.168.73.100:3306
  Source Schema         : scaffold
 
  Target Server Type    : MySQL
- Target Server Version : 80037 (8.0.37-0ubuntu0.24.04.1)
+ Target Server Version : 80037 (8.0.37-0ubuntu0.22.04.3)
  File Encoding         : 65001
 
- Date: 26/06/2024 00:51:28
+ Date: 26/06/2024 17:07:34
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for machine
+-- ----------------------------
+DROP TABLE IF EXISTS `machine`;
+CREATE TABLE `machine`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '机器名',
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '机器IP',
+  `port` smallint UNSIGNED NOT NULL DEFAULT 22 COMMENT '用于SSH链接的端口',
+  `operating_system` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主机操作系统',
+  `virtual` tinyint UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否为虚拟机（0：不是；1：是）',
+  `enabled` tinyint UNSIGNED NOT NULL COMMENT '是否可用（0：不可用；1：可用）',
+  `remark` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注信息',
+  `created_user` bigint UNSIGNED NULL DEFAULT NULL COMMENT '创建用户',
+  `created_time` datetime NOT NULL COMMENT '创建时间',
+  `latest_time` datetime NULL DEFAULT NULL COMMENT '最近登录时间',
+  `updated_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `updated_user` bigint NULL DEFAULT NULL COMMENT '修改用户',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '主机表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for resource_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `resource_auth`;
+CREATE TABLE `resource_auth`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '名称',
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `privileged` tinyint(1) NOT NULL COMMENT '是否为特权账号',
+  `enabled` tinyint(1) NOT NULL COMMENT '是否可用',
+  `machine_id` bigint UNSIGNED NOT NULL COMMENT '所属机器ID',
+  `remark` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注信息',
+  `latest_conn_time` datetime NULL DEFAULT NULL COMMENT '上次连接时间',
+  `effective_end_time` datetime NOT NULL DEFAULT ((now() + interval 1 year)) COMMENT '有效结束时间',
+  `created_time` datetime NULL DEFAULT (now()) COMMENT '创建时间',
+  `created_user` bigint NULL DEFAULT NULL COMMENT '创建用户',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '主机账号' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for sys_operation_log
@@ -36,11 +77,6 @@ CREATE TABLE `sys_operation_log`  (
   `operator` bigint NULL DEFAULT NULL COMMENT '操作人',
   `operated_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`uuid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户操作日志表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of sys_operation_log
--- ----------------------------
-INSERT INTO `sys_operation_log` VALUES ('FFDEB6F2CA4748FDBD82A4DF1AC10D3D', '', '/api/scaffold/hello', 'GET', 'test', 'username=nihao username: nihao', 9, '198.18.0.1', '0:0:0:0:0:0:0:1', 1, NULL, 1, '2024-06-26 00:48:32');
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户操作日志表' ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
